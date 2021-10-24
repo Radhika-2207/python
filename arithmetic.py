c3d7f4cf73828a9670eba6ddf55ea9d9d494f979
Radhika Goswami <70616966+Radhika-2207@users.noreply.github.com>
2021-10-24 17:09:22 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,32 @@
+#using a variable
+a = 3 + 3
+print(a)
+#using print straight away
+print(3+3)
+
+#trying a float
+print(0.1+0.2)
+
+#trying float and integer together
+print(0.1+22)
+
+#using underscore to see how python ignores it in numbers
+math_123 = 140_000
+print(math_123)
+
+#multiple assignment together
+x,y,z=1,2,3
+print(x,y,z)
+
+#printing them seperately to see if this actually works hehe
+print(x)
+print(y)
+print(z)
+
+#this is a comment
+"this is also a comment"
+"""
+multiple
+line
+comment
+"""
