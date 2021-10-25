4aeab97aeadcbee344280e762995167b93e92cf3
Radhika Goswami <70616966+Radhika-2207@users.noreply.github.com>
2021-10-25 07:03:01 +0530

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,16 @@
+#using sort to orgranize them in ascending order
+alpha = ['c','a','f','b','e']
+alpha.sort()
+print(alpha)
+
+#using reverse=true to sort them in descending order
+alpha.sort(reverse=True)
+print(alpha)
+
+#reverse is used to reverse the order of the string given
+alpha = ['c','a','f','b','e']
+alpha.reverse()
+print(alpha)
+
+#using sorted to temporarily sort the list
+print(sorted(alpha))
